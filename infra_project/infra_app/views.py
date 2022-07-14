4f6520a4093eaab5a94544d0ce6b7d2ddb9d4a09
Ильин Артём <joker9099@mail.ru>
2022-07-14 22:47:25 +0300

    workflow

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    context = 'У меня получилось!'
+    return HttpResponse('У меня получилось???')
 
 
 def second_page(request):
